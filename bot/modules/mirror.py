96d2a8e589ed6401ee5a86aa5f95301bdfe43f1e
Neranjana Prasad | Nandiya <30690726+NandiyaLive@users.noreply.github.com>
2020-07-25 11:53:51 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -114,9 +114,9 @@ class MirrorListener(listeners.MirrorListeners):
             LOGGER.info(f'Done Uploading {download_dict[self.uid].name()}')
             if INDEX_URL is not None:
                 share_url = requests.utils.requote_uri(f'{INDEX_URL}/{download_dict[self.uid].name()}')
-                if os.path.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
+                if os.path.isdir(f'<b>You have no access to the Cloud URL so please use the Index URL 🤒</b>\n\n{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
                     share_url += '/'
-                msg += f'\n\nIndex URL: <a href="{share_url}">here</a>\n<b>You have no access to the Cloud URL so please use the Index URL 🤒</b>'
+                msg += f'\nIndex URL: <a href="{share_url}">here</a>'
             if self.tag is not None:
                 msg += f'\ncc: @{self.tag}'
             try:
